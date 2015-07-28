e6d71d0fa1f5913e549fa27ac28498263c76f347
jainaman224 <jainaman224@gmail.com>
2015-07-28 10:52:00 +0530

    hey

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/aman/ap/venv"
+set -gx VIRTUAL_ENV "/home/aman/jainaman224/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
