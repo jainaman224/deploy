e7792e0b5fdbb0afc2b8eb7d10524d5a27e39ebd
jainaman224 <jainaman224@gmail.com>
2015-07-30 13:02:28 +0530

    am

--- games/static/calc.js
+++ games/static/calc.js
@@ -59,3 +59,19 @@ catch(e)
   c('Error');
 } 
 }
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '616786488424200',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
