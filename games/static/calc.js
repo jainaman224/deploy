e8bae5a3a6bc3611d43e4fdd67510c65140ffe15
jainaman224 <jainaman224@gmail.com>
2015-08-10 20:48:04 +0530

    yeah:-)

--- games/static/calc.js
+++ games/static/calc.js
@@ -74,4 +74,4 @@ window.fbAsyncInit = function() {
      js = d.createElement(s); js.id = id;
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  }(document, 'script', 'facebook-jssdk'));
